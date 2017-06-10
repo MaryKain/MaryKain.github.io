df694163c614723910e680f0ee9dda17c9a906cb
MaryKain <psyhelp911@gmail.com>
2017-06-10 21:33:24 +0300

    fix

--- hmJS-1/script.js
+++ hmJS-1/script.js
@@ -4,17 +4,17 @@
 
 var x = prompt ('Введите число');
 var n = prompt('Введите степень числа');
-    if (x!==0) alert ('Ошибка, введите целое число')
-else {
+    if (x<=0) alert ('Ошибка, введите целое число')
+    else {
 
-function pow (x,n){
-    var result=x;
-    for  (var i=1; i<n; i++){
-        result*=x;
-    }
-    return result;
+        function pow(x, n) {
+            var result = x;
+            for (var i = 1; i < n; i++) {
+                result *= x;
+            }
+            return result;
 
-}
+        }
     }
 
 alert (pow(x,n));
