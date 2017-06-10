3650c8c29891aa9a2c22a64e003b40f3a5bdcc5c
MaryKain <psyhelp911@gmail.com>
2017-06-10 19:33:50 +0300

    first part

---
+++ hmJS-1/script.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Marina on 10.06.2017.
+ */
+
+var x = prompt ('Введите число');
+var n = prompt('Введите степень числа');
+    if (x!==0) alert ('Ошибка, введите целое число')
+else {
+
+function pow (x,n){
+    var result=x;
+    for  (var i=1; i<n; i++){
+        result*=x;
+    }
+    return result;
+
+}
+    }
+
+alert (pow(x,n));
